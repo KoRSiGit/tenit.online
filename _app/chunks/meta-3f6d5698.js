14dcaec33d3d928c7a459edd8a38d8e39a5f0197
KoRSiGit <f8@ngs.ru>
2022-02-05 01:45:34 +0700

    Initial

---
+++ _app/chunks/meta-3f6d5698.js
@@ -0,0 +1,1 @@
+const t="https://tenit.online",n=["Tim","Young Journalist","Interesting","Journalist","Open Source"],s="I'm a young journalist aiming to build notes that spark joy. I collect notes about fun stuff, and sometimes write it down.",i="Tim - Young Journalist",o="https://tenit.online/images/site-preview.png";export{s as d,o as i,n as k,t as s,i as t};
